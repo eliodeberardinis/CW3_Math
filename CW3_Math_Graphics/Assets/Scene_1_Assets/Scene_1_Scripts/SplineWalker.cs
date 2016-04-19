e4910b563f56a3293e7a04bb3adb90f10f558636
Elio de Berardinis <eliodeb@gmail.com>
2016-04-19 12:18:17 +0100

    ggg

--- CW3_Math_Graphics/Assets/Scene_1_Assets/Scene_1_Scripts/SplineWalker.cs
+++ CW3_Math_Graphics/Assets/Scene_1_Assets/Scene_1_Scripts/SplineWalker.cs
@@ -3,11 +3,12 @@
 public class SplineWalker : MonoBehaviour
 {
 
-    public BezierSpline spline;
-    public float duration;
-    private float progress;
-    public bool lookForward;
+    public BezierSpline spline;  //Reference to the spline to which the object is attached to
+    public float duration;      //The time the object takes to go around the spline
+    private float progress;    //The amount of time that has passed since the object started moving along the spline
+    public bool lookForward;  //Variable to check if the object is facing the direction it is moving towards
 
+    //Enum for the walker mode of tracing the path. Once, in a loop or back and forth
     public enum SplineWalkerMode
     {
         Once,
@@ -17,11 +18,13 @@ public class SplineWalker : MonoBehaviour
 
     public SplineWalkerMode mode;
 
+    //Bool variable to check if the walker is facing the direction it is moving towards
     private bool goingForward = true;
 
     private void Update()
     {
-        if (goingForward)
+        //The update function refreshes at every frame taking into account the movement of the object along the spline according to its speed and duration and updates the progress variable
+        if (goingForward) //This is done either when the object is facing forward
         {
             progress += Time.deltaTime / duration;
             if (progress > 1f)
@@ -41,7 +44,7 @@ public class SplineWalker : MonoBehaviour
                 }
             }
         }
-        else
+        else //or not by inverting the sign
         {
             progress -= Time.deltaTime / duration;
             if (progress < 0f)
@@ -51,10 +54,12 @@ public class SplineWalker : MonoBehaviour
             }
         }
 
+        //updates the position of the object along the spline
         Vector3 position = spline.GetPoint(progress);
         transform.localPosition = position;
         if (lookForward)
         {
+            //Adjust the rotation of the object and forces to look forward if the relevant bool is set to true
             transform.LookAt(position + spline.GetDirection(progress));
         }
     }
